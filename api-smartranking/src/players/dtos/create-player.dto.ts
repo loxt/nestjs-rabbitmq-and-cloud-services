5e418dd6b91056f8ee363bdfea5d82eab981ddc8
loxt <emannuelloxt@hotmail.com>
2022-05-26 22:36:32 -0400

    feat: add create player dto input

--- api-smartranking/src/players/dtos/create-player.dto.ts
+++ api-smartranking/src/players/dtos/create-player.dto.ts
@@ -2,7 +2,4 @@ export class CreatePlayerDto {
   readonly phoneNumber: string;
   readonly email: string;
   readonly name: string;
-  readonly ranking: string;
-  readonly rankingPosition: number;
-  readonly photoUrl: string;
 }
