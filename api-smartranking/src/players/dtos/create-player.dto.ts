95e1d9b97d0d9badc125c8113aa38a5c426199cb
loxt <emannuelloxt@hotmail.com>
2022-05-28 00:51:41 -0400

    feat: add create player dto validation with class-validator

--- api-smartranking/src/players/dtos/create-player.dto.ts
+++ api-smartranking/src/players/dtos/create-player.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreatePlayerDto {
+  @IsNotEmpty()
   readonly phoneNumber: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly name: string;
 }
