e93c798bffdd74a0099b30cdca0af58068f4be0f
loxt <emannuelloxt@hotmail.com>
2022-05-28 00:16:41 -0400

    feat: add mongoose module

--- api-smartranking/src/app.module.ts
+++ api-smartranking/src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PlayersModule } from './players/players.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:123@cluster0.rvrs2ta.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    PlayersModule,
+  ],
 })
 export class AppModule {}
