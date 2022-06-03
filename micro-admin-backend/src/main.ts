5363ef8d9cb0f8ba89d8632c5300f8b3d0d2a4f4
loxt <emannuelloxt@hotmail.com>
2022-06-02 22:14:40 -0400

    feat: microservice bootstrap

--- micro-admin-backend/src/main.ts
+++ micro-admin-backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://user:QtJFBU8LdY2P@3.95.238.78/smartranking'],
+      queue: 'admin-backend',
+    },
+  });
+  await app.listen();
 }
-bootstrap();
+bootstrap().then(() => console.log('Microservice is listening...'));
