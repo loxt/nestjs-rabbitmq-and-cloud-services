e1bc62fd843e83a468215b1c10a79cedf8bf2bc4
loxt <emannuelloxt@hotmail.com>
2022-06-05 17:01:20 -0400

    feat: add timeout interceptor

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as momentTimezone from 'moment-timezone';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalInterceptors(new TimeoutInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
 
   Date.prototype.toJSON = function (): any {
